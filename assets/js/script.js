a296a2494e47d2c3c6947c47afd84b6e0ee921d8
NakamotoSean <kitsunedark150@gmail.com>
2026-03-17 15:01:01 -0300

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,2 @@
-const buttons = document.querySelectorAll('.choices button');
-const playerChoiceText = document.getElementById('player-choice');
+const computerChoiceText = document.getElementById('computer-choice');
+const winnerText = document.getElementById('winner');
